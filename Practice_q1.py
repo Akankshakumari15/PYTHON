189a9d74097a863f30a307d5530fbc69f27029a3
Akankshakumari15 <a.akanksha050915@gmail.com>
2025-10-10 11:33:09 +0530

    PY

---
+++ Practice_q1.py
@@ -0,0 +1,27 @@
+# sum of two numbers
+a = int(input("Enter the first no: "))
+b = float(input("Enter the second no: "))
+sum=a+b
+print("Sum: ",sum)
+
+
+
+# area of a square
+side= float(input("Enter square side: " ))
+print("Area: " , side*side)
+
+
+
+# input two float nos and print average
+p = float(input("Enter the first no: "))
+q = float(input("Enter the second no: "))
+print("Average: ", (p+q)/2)
+
+
+# input two nos. print true if a is greater than or equal to b if not false
+print(a>=b)
+
+
+    
+
+
