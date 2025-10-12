bddad39ee423dba86148d2ac9ee21b92fe9b2ab2
Akankshakumari15 <a.akanksha050915@gmail.com>
2025-10-12 22:27:26 +0530

    PY

---
+++ Operators.py
@@ -0,0 +1,52 @@
+# types of operators
+
+##arithemetic operators
+a=2
+b=5
+sum=a+b
+sub=a-b
+mul=a*b
+div=a/b
+print("sum: ",sum)
+print("sub: ",sub)
+print("mul: ",mul)
+print("div: ",div)    #always float representation
+
+# # relational operators
+# **   (a^b)
+# == equality check
+# != not equal to
+# >= greater than <=less than
+a=50
+b=20
+print(a**b)        #a^b
+print(a == b)      #false
+print(a != b)      #true
+print(a>=b)        #true
+print(a<=b)        #false
+
+## assignment operators
+num=10
+num=num+10   # num+=10
+num-=10
+num%=10
+num**=2
+print("sum: ",num)
+
+## logical operators
+# not   opposite answers
+print(not False)
+print(not True)
+# and   
+val1 =True
+val2 = True
+val3 = False
+print("and operator: ", val1 and val2)    #and gives true only when both are true
+print("and operator: ", val1 and val3)
+# or
+print("or operator: ", val1 or val2)    #or gives true only even when one is true
+print("or operator: ", val1 or val3)
+
+
+
+
