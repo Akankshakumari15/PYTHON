9acea684ce926f59015c82901280c7ca5bdee55e
Akankshakumari15 <a.akanksha050915@gmail.com>
2025-10-14 20:48:16 +0530

    PY

---
+++ Variables_DataTypes.py
@@ -0,0 +1,33 @@
+print(23)
+print(78+1)
+
+# variable=value
+name="akuu"
+price=9700.2
+print("My name is: ",name)    
+
+# = assignment operator
+# a=b    , value of b is assigned to a
+
+## rules for identifiers
+# uppercase lowercase digits underscore(no special characters)
+# cannot start with a number
+
+print(type(name))        #string
+print(type(price))       #float
+
+# data types
+# integers(47,-2,3) string("hello",'hiii','''byeee''') float(47.202) boolean(True,False) none(a=none)
+
+# none variable
+age = 23
+old =False
+a=None
+print(type(old))
+print(type(a))
+
+# # Keywords in python
+# and as assert break True False class continue family for from not or while yield lambda return try with def del
+
+# python is a case-sensitive language
+
