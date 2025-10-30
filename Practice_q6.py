59a9f22f94b5c6544188a3bee6ac63e564e1433e
Akankshakumari15 <a.akanksha050915@gmail.com>
2025-10-30 19:19:00 +0530

    PY

---
+++ Practice_q6.py
@@ -0,0 +1,10 @@
+# wap to print length of liat where list is a parameter
+def list_len(my_list):   
+    return len(my_list)
+
+numbers = [3, 5, 4, 3, 2, 22, 0]
+print("Length of list:", list_len(numbers))
+
+
+# print elements of list in a single line
+
