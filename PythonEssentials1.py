0038a382c4a74695e33d1598cc061dc908a3d183
Akankshakumari15 <a.akanksha050915@gmail.com>
2025-11-22 10:28:27 +0530

    Py

---
+++ PythonEssentials1.py
@@ -0,0 +1,4 @@
+# An element with an index equal to -1 is the last one in the list.
+numbers = [111, 7, 2, 1]
+print(numbers[-1])
+# Similarly, the element with an index equal to -2 is the one before last in the list.
